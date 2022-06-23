191fa920e1801d9f81f720ea474eeb46b330cecd
jamobrand <jamobrand@gmail.com>
2022-06-24 01:39:36 +0300

    new changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,11 +17,6 @@ module.exports = {
       options: {
         id: process.env.GATSBY_GOOGLE_TAGMANAGER_ID,
         includeInDevelopment: false,
-        defaultDataLayer: function () {
-          return {
-            pageType: window.pageType,
-          }
-        },
         enableWebVitalsTracking: true,
         //selfHostedOrigin: "https://jbclient.jamobrand.com",
       },
